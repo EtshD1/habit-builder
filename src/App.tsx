74a6e9555436ebf560ee08a91bcbf6d600071afa
EtshD1 <H79Pain@Gmail.com>
2021-06-12 19:18:26 +0200

    replace template code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles/main.scss";
+import "./styles/reset.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App dark">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat rerum odit, fugit quas unde, sapiente sit laboriosam distinctio possimus dignissimos earum quae mollitia consequuntur totam cumque, iure error vitae eos.
     </div>
   );
 }
