6c4d57764743e33eba12ea362a9a8645b76db917
EtshD1 <mohamed.h.abouelenin@gmail.com>
2021-06-18 21:51:07 +0200

    Add Form (logicless) and with incomplete styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./styles/main.scss";
 import "./styles/reset.css";
 import Navbar from "./components/Navbar";
 import Body from "./components/Body";
+import Form from './components/AddTaskForm';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Body />
+      <Form />
     </div>
   );
 }
---
+++ src/components/AddTaskForm.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+type habitValues = {
+  title: string,
+  description: string,
+  category: string
+}
+
+const intialFormValue: habitValues = {
+  title: "",
+  description: "",
+  category: ""
+}
+
+const Form = () => {
+  const [formValues, setFormValues] = useState(intialFormValue);
+
+  const controlledSetState = (item: { title?: string, description?: string, category?: string }) => {
+    setFormValues(ps => ({ ...ps, item }));
+  }
+
+  return (
+    <div id="formContainer">
+      <div>
+        <form>
+          <div>
+            <input type="text" placeholder="Title" onChange={(e) => controlledSetState({ title: e.target.value })} />
+            <input type="text" placeholder="Description" onChange={(e) => controlledSetState({ description: e.target.value })} />
+            <div>
+              <div className="dropdown">
+                <div>Category</div>
+                <div className="arrow">
+                  <svg width="24" height="14" viewBox="0 0 24 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10.9393 13.0607C11.5251 13.6464 12.4749 13.6464 13.0607 13.0607L22.6066 3.51472C23.1924 2.92893 23.1924 1.97919 22.6066 1.3934C22.0208 0.807611 21.0711 0.807611 20.4853 1.3934L12 9.87868L3.51472 1.3934C2.92893 0.807611 1.97919 0.807611 1.3934 1.3934C0.807611 1.97919 0.807611 2.92893 1.3934 3.51472L10.9393 13.0607ZM10.5 10V12H13.5V10H10.5Z" fill="black" />
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Form;
