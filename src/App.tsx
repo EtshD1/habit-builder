944c949520fd1801e4f70206eabf2aee0e82e277
EtshD1 <H79Pain@Gmail.com>
2021-06-19 22:24:05 +0200

    firebase boilerplate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import "./styles/main.scss";
 import "./styles/reset.css";
+
 import Navbar from "./components/Navbar";
 import Body from "./components/Body";
 import Form from "./components/AddTaskForm";
+
 import { useState, useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+import config from "./firebase.json";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import "firebase/analytics";
+
+firebase.initializeApp(config);
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 const getTheme = () => {
   const value = window.localStorage.getItem("theme");
@@ -14,9 +28,19 @@ const getTheme = () => {
   return window.matchMedia("(prefers-color-scheme: dark)").matches;
 };
 
+const signIn = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  auth.signInWithPopup(provider);
+};
+
+const signOut = () => auth.signOut();
+
 const App = () => {
   const [darkTheme, setDarkTheme] = useState(false);
   const [form, showForm] = useState(false);
+
+  const [user] = useAuthState(auth);
+  console.log(user);
 
   useEffect(() => {
     setDarkTheme(getTheme());
@@ -36,7 +60,13 @@ const App = () => {
   return (
     <div className={`App ${darkTheme ? "dark" : ""}`}>
       <Navbar {...{ changeTheme }} />
-      <Body add={() => showForm(true)} />
+      {user ? (
+        <Body add={() => showForm(true)} />
+      ) : (
+        <div id="signInBody">
+          <div>Sign In</div>
+        </div>
+      )}
       {form ? <Form close={() => showForm(false)} /> : ""}
     </div>
   );
