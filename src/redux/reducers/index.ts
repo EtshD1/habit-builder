4b0a8257b1a4c67188bc99fccaafd1e27a5fd9b6
EtshD1 <mohamed.h.abouelenin@gmail.com>
2021-06-27 00:14:11 +0200

    Replace found habit in redux instead of skip it for if the server changes some values, it will represent accurate info

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,17 +1,19 @@
 import { habit, HabitActionType, habitDispatches, userDispatches, UserActionType } from "../type";
 import firebase from "firebase";
-import { combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 
 const habitsReducer = (state: Array<habit> = [], action: HabitActionType) => {
   switch (action.type) {
     case habitDispatches.ADD:
       if (typeof action.payload === "object") {
         const id = action.payload.id;
-        const found = state.find(i=>  i.id === id);
-        if (!found) {
+        const found = state.findIndex(i => i.id === id);
+        if (found !== -1) {
+          return [...state.slice(0, found), action.payload, ...state.slice(found + 1)];
+        } else {
           return [...state, action.payload];
         }
-      } else console.warn("Payload supplied incorrectly\n")
+      } else console.warn("Payload supplied incorrectly\n");
       return [...state];
     case habitDispatches.CHECKIN:
       const newState = [...state];
@@ -30,7 +32,7 @@ const habitsReducer = (state: Array<habit> = [], action: HabitActionType) => {
   return state;
 }
 
-const userReducer = (state:firebase.User| null = null, action: UserActionType) => {
+const userReducer = (state: firebase.User | null = null, action: UserActionType) => {
   switch (action.type) {
     case userDispatches.LOGIN:
       return action.payload;
@@ -48,6 +50,6 @@ const allReducers = combineReducers({
 export default allReducers;
 
 export type storeType = {
-  user:firebase.User| null| undefined,
-  habits: Array<habit> 
+  user: firebase.User | null | undefined,
+  habits: Array<habit>
 }
